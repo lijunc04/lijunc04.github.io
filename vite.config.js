72c2863e5185aafe8e98dafaee594dff769f3dcd
lijuncai04 <jackcailijun@gmail.com>
2023-12-07 14:10:41 -0500

    edit: deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:"/lijunc04.github.io/"
+  plugins: [react()]
 })
  
